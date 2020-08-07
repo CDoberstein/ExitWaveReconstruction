d7be6a2b8b5d763b9602cf88ce60d53ae96e0350
Christian Doberstein <doberstein@aices.rwth-aachen.de>
2020-08-07 13:49:08 +0200

    Added support for reading dm3, q2bz, tiff, ser or png image files

--- Src/projects/ExitWaveReconstruction/LoadUtilities.h
+++ Src/projects/ExitWaveReconstruction/LoadUtilities.h
@@ -3,6 +3,7 @@
 
 #include "Image.h"
 #include "Parameters.h"
+#include "convertSERToQuoc.h"
 
 #include <dm3Import.h>
 #include <denoising.h>
@@ -88,20 +89,25 @@ void LoadAndConvertScalarArray ( qc::ScalarArray<RealType, qc::QC_2D>& image,
 /**
  * \brief Loads and processes an image series
  * 
- * The path variable is expected to point to a (existing or nonexisting)
- * .dm3 file containing the entire experimental image series. If the .dm3
- * file does not exist, individual images are searched for in a directory
- * with the name of the .dm3 file without the extension. The individual images
- * then have to be in .dm3 or .q2bz format and the numbering of the files
- * should be done according to Param.outputNumberingFormat (see example).
+ * The path variable may either point to a file or directory as
+ * documented in the template parameter file for the inputDataFile
+ * parameter (but not to a exit wave) or point to a nonexisting .dm3
+ * file.
  * 
- * The input images are assumed to be in real space coordinates without a
- * frequency shift.
+ * If the path variable points to a nonexisting .dm3 file, individual
+ * images are searched for in a directory with the name of the .dm3
+ * file without the extension. The individual images then have to be
+ * in .dm3 or .q2bz format and the numbering of the files should be
+ * done according to Param.outputNumberingFormat (see example).
+ * 
+ * The input images are assumed to be in real space coordinates
+ * without a frequency shift.
  * 
  * Example: path = /Volumes/Data/FocusSeries.dm3
  *  -> Individual images: /Volumes/Data/FocusSeries/ImageXXX.q2bz
  *                     or /Volumes/Data/FocusSeries/ImageXXX.dm3
- *     where XXX is replaced by the image number in the format Param.outputNumberingFormat
+ *     where XXX is replaced by the image number in the format
+ *     Param.outputNumberingFormat
  */
 template <typename RealType>
 void LoadInputImageSeries ( vector<Image<RealType>>& ImageSeries,
@@ -111,70 +117,202 @@ void LoadInputImageSeries ( vector<Image<RealType>>& ImageSeries,
   if ( verbose )
     cerr << "\tSearching for input images...";
   
-  // Check if path points to a .dm3 file
-  if ( path.length ( ) < 4 || path.substr ( path.length ( ) - 4 ) != string ( ".dm3" ) )
-    throw aol::Exception ( "Invalid path!", __FILE__, __LINE__ );
+  string imagePath;
+  bool q2bz_image_input = false;
   
-  /** Check if .q2bz images exist and extract them from .dm3 file(s) if necessary **/
-  string imagePath = path.substr ( 0, path.length ( ) - 4 ) + "/";
-  
-  // Path to the first image
-  char image0_id[1024];
-  sprintf ( image0_id, Param.outputNumberingFormat.c_str ( ), 0 );
-  string image0 = imagePath + "Image" + image0_id;
-  
-  if ( aol::fileExists ( image0 + ".q2bz" ) ) {
-    if ( verbose )
-      cerr << " found .q2bz files." << endl;
+  // Check if path points to a (existing or nonexisting) .dm3 file
+  if ( path.length ( ) >= 4 && path.substr ( path.length ( ) - 4 ) == string ( ".dm3" ) ) {
+    /** Check if .q2bz images exist and extract them from .dm3 file(s) if necessary **/
+    imagePath = path.substr ( 0, path.length ( ) - 4 ) + "/";
+    
+    // Path to the first image
+    char image0_id[1024];
+    sprintf ( image0_id, Param.outputNumberingFormat.c_str ( ), 0 );
+    string image0 = imagePath + "Image" + image0_id;
+    
+    if ( aol::fileExists ( image0 + ".q2bz" ) ) {
+      q2bz_image_input = true;
+      if ( verbose )
+        cerr << " found .q2bz files." << endl;
+    } else {
+      // Check if .dm3 images exist
+      if ( aol::fileExists ( image0 + ".dm3" ) ) {
+        if ( verbose )
+          cerr << " found individual .dm3 files." << endl;
+        
+        // Convert .dm3 images to .q2bz format
+        for ( int i = 0; ; i++ ) {
+          if ( verbose )
+            cerr << "\r\t\tConverting image " << i+1 << "...";
+          
+          char id[1024];
+          sprintf ( id, Param.outputNumberingFormat.c_str ( ), i );
+          string current_img = imagePath + "Image"  + id + ".dm3";
+          
+          if ( !aol::fileExists ( current_img ) )
+            break;
+          
+          qc::DM3Reader reader ( current_img );
+          qc::ScalarArray<RealType, qc::QC_2D> current_image_data;
+          
+          reader.exportDataToScalarArray ( current_image_data );
+          
+          current_image_data.saveToFile ( ( imagePath + "Image"  + id + ".q2bz" ).c_str ( ) );
+        }
+        
+        if ( verbose )
+          cerr << "\r\t\tConverting images finished.     " << endl;
+      } else {
+        // Check if a large .dm3 file containing all images exists
+        if ( aol::fileExists ( path ) ) {
+          MakeDirectories ( imagePath, false );
+          
+          // Read .dm3 file and extract slices
+          qc::DM3Reader reader ( path );
+          qc::ScalarArray<RealType, qc::QC_3D> image_data;
+          
+          reader.exportDataToScalarArray ( image_data );
+          
+          if ( verbose )
+            cerr << " found a .dm3 file containing " << image_data.getNumZ ( ) << " images." << endl
+                 << "\t\tConverting images...";
+          
+          image_data.saveSlices ( ( imagePath + "Image"  + Param.outputNumberingFormat + ".q2bz" ).c_str ( ), qc::QC_Z, qc::PGM_DOUBLE_BINARY );
+          
+          if ( verbose )
+            cerr << " done." << endl;
+        } else
+          throw aol::Exception ( "Unable to find focus image series!", __FILE__, __LINE__ );
+      }
+    }
   } else {
-    // Check if .dm3 images exist
-    if ( aol::fileExists ( image0 + ".dm3" ) ) {
+    // path does not point to a .dm3 file so it has to point to a directory
+    // containing individual TEM images of one of the following formats:
+    //   (1) q2bz
+    //   (2) dm3
+    //   (3) tiff
+    //   (4) ser
+    //   (5) png
+    if ( !aol::directoryExists ( path ) )
+      throw aol::Exception ( "Invalid input data path!", __FILE__, __LINE__ );
+    
+    imagePath = path;
+    if ( imagePath[imagePath.length ( ) - 1] != '/' )
+      imagePath += '/';
+    
+    /** Determine the file type of the images and convert them to .q2bz if necessary **/
+    // Path to the first image
+    char image0_id[1024];
+    sprintf ( image0_id, Param.outputNumberingFormat.c_str ( ), 0 );
+    string image0 = imagePath + "Image" + image0_id;
+    
+    if ( aol::fileExists ( image0 + ".q2bz" ) ) {
+      // q2bz
+      q2bz_image_input = true;
+      if ( verbose )
+        cerr << " found .q2bz files." << endl;
+    } else if ( aol::fileExists ( image0 + ".dm3" ) ) {
+      // dm3
       if ( verbose )
         cerr << " found individual .dm3 files." << endl;
       
-      // Convert .dm3 images to .q2bz format
-      for ( int i = 0; ; i++ ) {
+      // Convert .dm3 images to the .q2bz format
+      for ( int i = 0; i < Param.N ; i++ ) {
         if ( verbose )
-          cerr << "\r\t\tConverting image " << i+1 << "...";
+          cerr << "\r\t\tConverting image " << i+1 << "/" << Param.N << "...";
         
         char id[1024];
         sprintf ( id, Param.outputNumberingFormat.c_str ( ), i );
-        string current_img = imagePath + "Image"  + id + ".dm3";
-        
-        if ( !aol::fileExists ( current_img ) )
-          break;
+        string current_img = imagePath + "Image" + id + ".dm3";
         
         qc::DM3Reader reader ( current_img );
         qc::ScalarArray<RealType, qc::QC_2D> current_image_data;
         
         reader.exportDataToScalarArray ( current_image_data );
         
-        current_image_data.saveToFile ( ( imagePath + "Image"  + id + ".q2bz" ).c_str ( ) );
+        current_image_data.saveToFile ( ( imagePath + "Image" + id + ".q2bz" ).c_str ( ) );
       }
       
       if ( verbose )
-        cerr << "\r\t\tConverting images finished.     " << endl;
+        cerr << "\r\t\tConverting images finished.          " << endl;
+    } else if ( aol::fileExists ( image0 + ".tiff" ) || aol::fileExists ( image0 + ".TIFF" ) ) {
+      // tiff
+      string extension = ".tiff";
+      if ( aol::fileExists ( image0 + ".TIFF" ) )
+        extension = ".TIFF";
+      
+      if ( verbose )
+        cerr << " found " << extension << " files." << endl;
+      
+      // Convert .tiff images to the .q2bz format
+      for ( int i = 0; i < Param.N ; i++ ) {
+        if ( verbose )
+          cerr << "\r\t\tConverting image " << i+1 << "/" << Param.N << "...";
+        
+        char id[1024];
+        sprintf ( id, Param.outputNumberingFormat.c_str ( ), i );
+        string current_img = imagePath + "Image" + id + extension;
+        
+        qc::ScalarArray<RealType, qc::QC_2D> current_image_data;
+        current_image_data.loadTIFF ( current_img.c_str ( ) );
+        
+        current_image_data.saveToFile ( ( imagePath + "Image" + id + ".q2bz" ).c_str ( ) );
+      }
+      
+      if ( verbose )
+        cerr << "\r\t\tConverting images finished.          " << endl;
+    } else if ( aol::fileExists ( image0 + ".ser" ) || aol::fileExists ( image0 + ".SER" ) ) {
+      // ser
+      string extension = ".ser";
+      if ( aol::fileExists ( image0 + ".SER" ) )
+        extension = ".SER";
+      
+      if ( verbose )
+        cerr << " found " << extension << " files." << endl;
+      
+      // Convert .ser images to the .q2bz format
+      for ( int i = 0; i < Param.N ; i++ ) {
+        if ( verbose )
+          cerr << "\r\t\tConverting image " << i+1 << "/" << Param.N << "...";
+        
+        char id[1024];
+        sprintf ( id, Param.outputNumberingFormat.c_str ( ), i );
+        string current_img = imagePath + "Image" + id + extension;
+        
+        convertSERToQuoc ( current_img, imagePath + "Image" + id + ".q2bz" );
+      }
+      
+      if ( verbose )
+        cerr << "\r\t\tConverting images finished.          " << endl;
+    } else if ( aol::fileExists ( image0 + ".png" ) || aol::fileExists ( image0 + ".PNG" ) ) {
+      // png
+      string extension = ".png";
+      if ( aol::fileExists ( image0 + ".PNG" ) )
+        extension = ".PNG";
+      
+      if ( verbose )
+        cerr << " found " << extension << " files." << endl;
+      
+      // Convert .png images to the .q2bz format
+      for ( int i = 0; i < Param.N ; i++ ) {
+        if ( verbose )
+          cerr << "\r\t\tConverting image " << i+1 << "/" << Param.N << "...";
+        
+        char id[1024];
+        sprintf ( id, Param.outputNumberingFormat.c_str ( ), i );
+        string current_img = imagePath + "Image" + id + extension;
+        
+        qc::ScalarArray<RealType, qc::QC_2D> current_image_data;
+        current_image_data.loadPNG ( current_img.c_str ( ) );
+        
+        current_image_data.saveToFile ( ( imagePath + "Image" + id + ".q2bz" ).c_str ( ) );
+      }
+      
+      if ( verbose )
+        cerr << "\r\t\tConverting images finished.          " << endl;
     } else {
-      // Check if a large .dm3 file containing all images exists
-      if ( aol::fileExists ( path ) ) {
-        MakeDirectories ( imagePath, false );
-        
-        // Read .dm3 file and extract slices
-        qc::DM3Reader reader ( path );
-        qc::ScalarArray<RealType, qc::QC_3D> image_data;
-        
-        reader.exportDataToScalarArray ( image_data );
-        
-        if ( verbose )
-          cerr << " found a .dm3 file containing " << image_data.getNumZ ( ) << " images." << endl
-               << "\t\tConverting images...";
-        
-        image_data.saveSlices ( ( imagePath + "Image"  + Param.outputNumberingFormat + ".q2bz" ).c_str ( ), qc::QC_Z, qc::PGM_DOUBLE_BINARY );
-        
-        if ( verbose )
-          cerr << " done." << endl;
-      } else
-        throw aol::Exception ( "Unable to find focus image series!", __FILE__, __LINE__ );
+      throw aol::Exception ( "Invalid input data path, invalid image numbering format or unknown extension!",
+                             __FILE__, __LINE__ );
     }
   }
   
@@ -207,7 +345,7 @@ void LoadInputImageSeries ( vector<Image<RealType>>& ImageSeries,
     
     // Ask for permission to overwrite the .q2bz images if the images are not found in a
     // different format elsewhere
-    if ( !( aol::fileExists ( path ) || aol::fileExists ( image0 + ".dm3" ) ) ) {
+    if ( q2bz_image_input ) {
       cerr << endl << "Warning: Inpainting will overwrite the .q2bz image files with the inpainted images!" << endl
            << "Continue? (y/n)";
       
